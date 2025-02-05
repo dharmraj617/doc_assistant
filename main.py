427f3d1d2d820009d975f7dac334ce5a3eef260b
Dharmraj Patil <112186744+dharmraj617@users.noreply.github.com>
2025-02-05 22:49:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import os
-
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 import openai
-from openai import api_key
-from os import environ
+import os
 
 os.environ["OPENAI_API_KEY"] = "Your key"
 
-loader = PyPDFLoader('resumes.pdf')
+loader = PyPDFLoader('your pdf file path')
 documents = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
@@ -25,10 +22,7 @@ document_texts = "\n\n".join([doc.page_content for doc in relevant_docs])
 
 prompt = f"Based on the following documents, answer the query: {query}\n\n{document_texts}"
 
-# Initialize LLM
 llm = OpenAI(openai_api_key=os.environ["OPENAI_API_KEY"])
-
-# Generate response
 response = llm.invoke(prompt)
 
 print(response)
